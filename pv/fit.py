fbbcfc960bbd5a869e357954f86754ad8d13817a
Alex <agkim@lbl.gov>
2018-08-16 11:02:50 -0700

    presumably make log det calculation faster by using evalues of a symmetric matrix

--- pv/fit.py
+++ pv/fit.py
@@ -6,37 +6,37 @@ import emcee
 hg = HostGalaxies(path='../test/')
 xi = numpy.loadtxt("../test/pvlist.1234.xi")
 
+#immutables that enter the calculation
+ngal = len(hg.galaxies['mB'])
+
 m_eff =[]
 for  m, n in zip(hg.galaxies['mB'],hg.galaxies['nsne']):
     m_eff.append(m.sum()/n)
 m_eff=numpy.array(m_eff)
-
-#immutables that enter the calculation
-ngal = len(hg.galaxies['mB'])
 Deltam=m_eff- hg.galaxies['mB_expected']
 
 def lnprob(theta):
     A, M, sigma = theta
+    if (A <=0 or sigma <0):
+        return -numpy.inf
     C = A*numpy.array(xi)
     numpy.fill_diagonal(C,C.diagonal()+ sigma**2/hg.galaxies['nsne'])
     Cinv= numpy.linalg.inv(C)
     Croots = scipy.linalg.sqrtm(C)
-    logdetC = 2*numpy.log(Croots.diagonal()).sum()
-
+    logdetC = numpy.log(numpy.linalg.eigvalsh(C)).sum()
     mterm  = Deltam-M
 
-    lp = -logdetC - 0.5* ( mterm.T @ (Cinv @ mterm) )
+    lp = -0.5*halflogdetC - 0.5* ( mterm.T @ (Cinv @ mterm) )
     if not numpy.isfinite(lp):
         return -np.inf
     return lp
 
-ndim, nwalkers = 3, 10
+ndim, nwalkers = 3, 12
 sig = numpy.array([0.1,0.01,0.01])
 
-p0 = [numpy.array([1,0,0.1])+numpy.random.uniform(low = -sig, high=sig) for i in range(nwalkers)]
-
+p0 = [numpy.array([1,0,0.08])+numpy.random.uniform(low = -sig, high=sig) for i in range(nwalkers)]
 sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob)
-sampler.run_mcmc(p0, 100)
+sampler.run_mcmc(p0, 2000)
 
 import pickle
-pickle.dump(sampler.flatchain, open( "emcee.pkl", "wb" ) )
+pickle.dump(sampler.chain, open( "emcee.pkl", "wb" ) )
