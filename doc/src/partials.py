78f811a0aa394e5dacd6de58cfd602cc597ef008
Alex Kim <agkim@lbl.gov>
2019-01-24 23:40:24 -0800

    forging on

--- doc/src/partials.py
+++ doc/src/partials.py
@@ -270,7 +270,11 @@ def kintegral(z,zmax,ng,duration,sigm):
     blsigM = numpy.trapz(matter[:,0][w]**2*blsigM[w],matter[:,0][w])
     ll_vonly = numpy.trapz(matter[:,0][w]**2*ll_vonly[w],matter[:,0][w])
 
-    return ll,bb,bl, lls,bbs,bls,ll_ind,bb_ind,bl_ind, llsigM,bbsigM,blsigM,ll_vonly
+    llkmax = numpy.interp(kmax,matter[:,0], matter[:,0]**2*ll)
+    bbkmax = numpy.interp(kmax,matter[:,0], matter[:,0]**2*bb)
+    blkmax = numpy.interp(kmax,matter[:,0], matter[:,0]**2*bl)
+
+    return ll,bb,bl, lls,bbs,bls,ll_ind,bb_ind,bl_ind, llsigM,bbsigM,blsigM,ll_vonly, llkmax,bbkmax,blkmax
 
 # utility numbers
 zmax_zint = 0.3
@@ -301,9 +305,12 @@ def zintegral(zmax,ng,duration,sigm):
     bbsigM=numpy.zeros(len(rs))
     blsigM=numpy.zeros(len(rs))
     ll_vonly=numpy.zeros(len(rs))
+    llkmax=numpy.zeros(len(rs))
+    bbkmax=numpy.zeros(len(rs))
+    blkmax=numpy.zeros(len(rs))
 
     for i in range(len(rs)):
-        ll[i],bb[i],bl[i],lls[i],bbs[i],bls[i],ll_ind[i],bb_ind[i],bl_ind[i],llsigM[i],bbsigM[i],blsigM[i],ll_vonly[i] = kintegral(zs[i],zmax,ng,duration,sigm)
+        ll[i],bb[i],bl[i],lls[i],bbs[i],bls[i],ll_ind[i],bb_ind[i],bl_ind[i],llsigM[i],bbsigM[i],blsigM[i],ll_vonly[i],llkmax[i],bbkmax[i],blkmax[i] = kintegral(zs[i],zmax,ng,duration,sigm)
 
     ll = numpy.trapz(rs**2*ll,rs)
     bb = numpy.trapz(rs**2*bb,rs)
@@ -318,6 +325,9 @@ def zintegral(zmax,ng,duration,sigm):
     bbsigM = numpy.trapz(rs**2*bbsigM,rs)
     blsigM = numpy.trapz(rs**2*blsigM,rs)
     ll_vonly = numpy.trapz(rs**2*ll_vonly,rs)
+    llkmax = numpy.trapz(rs**2*llkmax,rs)
+    bbkmax = numpy.trapz(rs**2*bbkmax,rs)
+    blkmax = numpy.trapz(rs**2*blkmax,rs)
 
     # a,b,c,d,e,f,g,h,i = kintegral(rs[0],zmax,ng,duration,sigm)
     # deltars = rs[1]-rs[0]
@@ -355,11 +365,11 @@ def zintegral(zmax,ng,duration,sigm):
     # ll_ind=ll_ind+0.5*g*rs[-1]**2*deltars
     # bb_ind=bb_ind+0.5*h*rs[-1]**2*deltars
     # bl_ind=bl_ind+0.5*i*rs[-1]**2*deltars
-    return ll, bb, bl, lls,bbs,bls,ll_ind, bb_ind, bl_ind, llsigM,bbsigM,blsigM,ll_vonly
+    return ll, bb, bl, lls,bbs,bls,ll_ind, bb_ind, bl_ind, llsigM,bbsigM,blsigM,ll_vonly, llkmax,bbkmax,blkmax
 
 
 def set2():
-    fig,(ax) = plt.subplots(1, 1, figsize=(8*.85,6*.85))
+    fig,(ax) = plt.subplots(1, 1, figsize=(8*.8,6*.8))
     durations = [2,10]
     labels = ['Two Years','Ten Years']
     colors = ['red','black']
@@ -381,7 +391,7 @@ def set2():
             for z,r in zip(zs,rs):
                 a=1./(1+z)
                 drdz = 1/numpy.sqrt(OmegaM0/a**3 + (1-OmegaM0)) # 1/H
-                _,_,_, f00s,f11s,f10s,_,_,_,_,_,_,_ = kintegral(z,zmax,ng,duration,sigm)
+                _,_,_, f00s,f11s,f10s,_,_,_,_,_,_,_,_,_,_ = kintegral(z,zmax,ng,duration,sigm)
                 dvardz.append(finvp(f00,f11,f10,f00s,f11s,f10s))
                 dvardz[-1] = dvardz[-1]/r**2
                 dvardz[-1] = dvardz[-1]*drdz  # z now has units of 100 km/s
@@ -396,11 +406,11 @@ def set2():
     plt.savefig('dvardz.png')
     plt.clf()
 
-set2()
+# set2()
 
 
 def set1():
-    fig,(ax) = plt.subplots(1, 1, figsize=(8*.85,6*.85))
+    fig,(ax) = plt.subplots(1, 1, figsize=(8*.8,6*.8))
     zmaxs = numpy.exp(numpy.arange(numpy.log(0.05),numpy.log(.300001),numpy.log(.3/.05)/8))
     durations = [2,10]
     labels = ['Two Years','Ten Years']
@@ -409,24 +419,28 @@ def set1():
     var_ind=[]
     dvardsigM = []
     var_vonly=[]
+    dvardkmax = []
     for duration,label in zip(durations,labels):
         v_=[]
         vind_=[]
         vvonly_=[]
         dv_=[]
         dvsigM_=[]
+        dvdkmax_=[]
         for zmax in zmaxs:
-            f00,f11,f10, f00s,f11s,f10s,f00_ind,f11_ind,f10_ind, f00sigM,f11sigM,f10sigM,f00_vonly = zintegral(zmax,ng,duration,sigm)
+            f00,f11,f10, f00s,f11s,f10s,f00_ind,f11_ind,f10_ind, f00sigM,f11sigM,f10sigM,f00_vonly, f00kmax,f11kmax,f10kmax = zintegral(zmax,ng,duration,sigm)
             dv_.append(finvp(f00,f11,f10,f00s,f11s,f10s))
             v_.append(numpy.linalg.inv(numpy.array([[f00,f10],[f10,f11]]))[0,0])
             vind_.append(numpy.linalg.inv(numpy.array([[f00_ind,f10_ind],[f10_ind,f11_ind]]))[0,0])
             vvonly_.append(1./f00_vonly)
             dvsigM_.append(finvp(f00,f11,f10,f00sigM,f11sigM,f10sigM))
+            dvdkmax_.append(finvp(f00,f11,f10,f00kmax,f11kmax,f10kmax))
         var.append(numpy.array(v_)*2*3.14/.75) #3/4
         dvards.append(numpy.array(dv_)*2*3.14/.75)
         var_ind.append(numpy.array(vind_)*2*3.14/.75) #3/4
         dvardsigM.append(numpy.array(dvsigM_)*2*3.14/.75)
-        var_vonly.append(numpy.array(vvonly_)*2*3.14/.75) #3/4    
+        var_vonly.append(numpy.array(vvonly_)*2*3.14/.75) #3/4   
+        dvardkmax.append(numpy.array(dvdkmax_)*2*3.14/.75)
  
     plt.plot(zmaxs,numpy.sqrt(var[0]),label='Two Years',color='red')
     plt.plot(zmaxs,numpy.sqrt(var_ind[0]),label='Two Years, Independent Surveys',color='red',ls=':')  
@@ -465,4 +479,14 @@ def set1():
     plt.savefig('dvardsigM.png')
     plt.clf()
 
+    plt.plot(zmaxs,-dvardkmax[0]/2/numpy.sqrt(var[0]),label='Two Years',color='red')
+    plt.plot(zmaxs,-dvardkmax[1]/2/numpy.sqrt(var[1]),label='Ten Years',color='black')
+    plt.legend()
+    # plt.yscale("log", nonposy='clip')
+    plt.ylim((0,plt.ylim()[1]))
+    plt.xlabel(r'$z_{max}$')
+    plt.ylabel(r'$|\frac{d\sigma_{\gamma}}{dk_{max}}|$')
+    plt.tight_layout()
+    plt.savefig('dvardkmax.png')
+    plt.clf()
 set1()
