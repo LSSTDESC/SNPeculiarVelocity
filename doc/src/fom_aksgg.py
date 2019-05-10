0d76e5a9a4a79d26c7f452fecff6683dd5538ba3
SGontcho <satya.gontcho@gmail.com>
2019-05-10 01:15:42 -0400

    SNPV FoM for OSTF v2

--- doc/src/fom_aksgg.py
+++ doc/src/fom_aksgg.py
@@ -38,54 +38,50 @@ for root, dirs, files in os.walk("Footprints/"): # for 10 years footprints
             rt.append(root[root.rfind('/')+1:])
 
 rt = np.array(rt)
+#print('len(rt)',len(rt))
 #print('rt',rt)
 
 skyfrac=np.array(skyfrac)
 #print("skyfrac",skyfrac)
 
-#w=np.where(rt == 'baseline2018a')[0]
-#skyfrac_ref= skyfrac[w]
-#print(skyfrac_ref)
-
-fom_ref=partials.surveyFOM(skyfrac_ref,10)
+fom_ref=1./partials.surveyFOM(skyfrac_ref,10)
 #print("fom_ref",fom_ref)
-fom_10 = partials.surveyFOM(skyfrac,10) / fom_ref
+fom_10 = (1./partials.surveyFOM(skyfrac,10)) / fom_ref
 #print(fom_10)
-
-rt=rt
 
 so =np.argsort(rt)
 dtp = []
 list = []
 
-for index in range(len(rt)):
+for index in range(len(rt)-1):
     list= (so[index],fom_10[so[index]])
     dtp.append((list))
 
 #print("liste=",dtp)
 dtype = [('cadence_argsort', float), ('fom_10', float)]
 to_sort = np.array(dtp, dtype=dtype)
-to_plot = np.sort(to_sort,order="fom_10") 
+to_plot = np.sort(to_sort,order="fom_10")
 
 tp_cadence = []
 tp_fom_10 = [] 
 
-for index in range(len(to_plot)):
-    i = len(to_plot) - index
-    tp_cadence.append(rt[int(to_plot[i][0])])
-    tp_fom_10.append(to_plot[i][1])
+for index in range(len(dtp)):
+    i = len(dtp) - index -1
+    a=to_plot[i]
+    tp_cadence.append(rt[int(a[0])])
+    tp_fom_10.append(a[1])
 
-print("Footprints : ", tp_cadence)
-print("FoM over 10 years : ", tp_fom_10)
+#print("Footprints : ", tp_cadence)
+#print("FoM over 10 years : ", tp_fom_10)
 
-#fig = plt.figure(figsize=(10,15))
-#ax = fig.add_subplot(1, 1, 1)
-#plt.barh(tp_cadence,tp_fom_10, alpha=0.7, color="grey")
-#plt.hlines(1, 0, 10, colors='k', linestyles='-', label='baseline2018a')
-#plt.xticks(rotation=0,fontsize=20)
-#plt.yticks(fontsize=20)
-#plt.tick_params(length=10,width=1.2)
-#plt.xlim(-0.5,12.5)
-#plt.title('Gamma FoM ',fontsize=25)
-#plt.savefig('FOM_Gamma_OSTF.pdf')
-#plt.show()
+fig = plt.figure(figsize=(10,15))
+ax = fig.add_subplot(1, 1, 1)
+plt.barh(tp_cadence,tp_fom_10, alpha=0.65, color="black")
+plt.xticks(rotation=0,fontsize=20)
+plt.yticks(fontsize=20)
+plt.tick_params(length=10,width=1.2)
+plt.xlabel("$\sigma_\gamma^{-1}$(cadence) / $\sigma_\gamma^{-1}$(baseline2018a)",fontsize=20)
+plt.ylim(0.5,13.5)
+plt.show()
+plt.savefig('FOM_Gamma_OSTF_new.pdf')
+
